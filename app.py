af3b58eec1dbdba6bf6a9d8c682c28379ce9ab60
ChrsWrmr <christoph.warmer@gmail.com>
2023-08-23 22:43:32 +0200

    first commit

---
+++ app.py
@@ -0,0 +1,14 @@
+import dash
+import dash_html_components as html
+
+# Initialize the Dash app
+app = dash.Dash(__name__)
+
+# Define the layout of the app
+app.layout = html.Div([
+    html.H1("Awesome Backtester")
+])
+
+# Run the app
+if __name__ == '__main__':
+    app.run_server(debug=True)
