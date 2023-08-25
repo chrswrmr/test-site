0381d20c5dcc3c14c754dfe034a56bd1068144c1
ChrsWrmr <christoph.warmer@gmail.com>
2023-08-25 22:36:15 +0200

    icon

--- ui.py
+++ ui.py
@@ -3,7 +3,7 @@ from dash import html
 
 app = dash.Dash(__name__)
 
-app.layout = html.H1("Awesome Backtester!!")
+app.layout = html.H1("🤖 Awesome Backtester!!")
 
 if __name__ == '__main__':
     app.run_server(debug=False)
